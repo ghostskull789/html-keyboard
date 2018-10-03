54cf30b51851d98ba0ad3347061eb20dffd5724c
Prateek Gupta <prateekgupta1198@gmail.com>
2018-10-03 21:23:20 +0530

    del button works now
    and you can click on buttons and type

--- main.js
+++ main.js
@@ -4,10 +4,37 @@ function toggle() {
     element.classList.toggle("body-invert");   
 }
 
-function type(){
-    var e = document.getElementById("key");
-    var text = e.getAttribute("data-key-text");
-    // alert(text);
-    document.getElementById("textt").innerHTML = text;
-}
+(function(){
+    var e,c;
+
+    function init(){
+         e = document.getElementById("keyboard").getElementsByClassName('keyboard__key');
+    
+        for(c=0;c<e.length;c++) {
+               e[c].addEventListener('onclick',makeClickHandler(c));
+
+            }
+         //alert(e);
+    }
+
+    function makeClickHandler(c) {
+            
+        e[c].onclick=function() {
+            if(e[c].id=='del'){
+                document.getElementById('text').value = '';
+            }
+            else if(e[c].id=='backspace'){
+                document.getElementById('text').value = '';
+            }
+            else{
+            document.getElementById('text').value+=e[c].getAttribute("data-key-text");
+            }
+        };
+        
+    }
+
+    window.addEventListener?
+    window.addEventListener('load',init,false):
+    window.attachEvent('onload',init);
+ })();
 // setInterval(toggle,180); 
